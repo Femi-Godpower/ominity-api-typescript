ebbf43ceac65eeb6e48aa7bbb5a87f6dc1351209
Femi-Godpower <femigodpower@gmail.com>
2026-02-06 05:24:18 +0100

    Implemented - CMS api schemas

--- src/models/operations/index.ts
+++ src/models/operations/index.ts
@@ -2,5 +2,10 @@ export * from "./commerce-products.js";
 export * from "./commerce-categories.js";
 export * from "./commerce-carts.js";
 export * from "./commerce-cart-items.js";
+export * from "./cms-components.js";
+export * from "./cms-layouts.js";
+export * from "./cms-pages.js";
+export * from "./cms-menus.js";
 export * from "./commerce-invoices.js";
+
 
